fb8ce6eaee9554bbe498d124330724f6b80fbd86
RodriguezSN <gamer.infante51@gmail.com>
2024-03-10 15:16:45 -0300

    0.0.2 agregue comentarios

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
@@ -10,9 +10,22 @@ const server = express();
 
 server.name = 'API';
 
+/*
+? Configura el middleware bodyParser.urlencoded para analizar los cuerpos de las
+? solicitudes entrantes en un middleware antes de los manejadores de ruta, 
+? lo que permite acceder a los datos enviados en el cuerpo de la solicitud a través
+? de req.body. La opción extended: true permite el análisis de datos más complejos, 
+? como objetos y matrices, mientras que limit: '50mb' establece el tamaño máximo del
+? cuerpo de la solicitud a 50 megabytes.
+*/
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
+/*
+? Configura el middleware cookieParser para analizar las cookies de las solicitudes entrantes en un middleware 
+? antes de los manejadores de ruta, lo que permite acceder a las cookies a través de req.cookies
+*/
 server.use(cookieParser());
+
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
@@ -23,7 +36,12 @@ server.use((req, res, next) => {
 });
 
 server.use('/', routes);
-
+/*
+? Dentro del middleware, se establece el estado de la respuesta HTTP basado en el estado del
+? error capturado (err.status o 500 por defecto) y se envía el mensaje de error (err.message o el 
+? objeto de error err en sí mismo) como respuesta. Esto permite que el cliente reciba información 
+? sobre el error que ocurrió.
+*/
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
--- server/src/db.js
+++ server/src/db.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
+/*
+? Importa el módulo path, que proporciona utilidades para trabajar con rutas de archivos y directorios
+*/
 const path = require('path');
+/*
+? Extrae las variables de entorno del objeto process.env
+*/
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
@@ -12,29 +18,48 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
 });
 const basename = path.basename(__filename);
 
+/*
+? Define un arreglo modelDefiners para almacenar definiciones de modelos. 
+*/
 const modelDefiners = [];
-
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+/*
+? Luego, lee todos los archivos en el directorio /models, filtra los archivos que no son .js o el archivo actual, 
+? y requiere cada uno de ellos, agregándolos al arreglo modelDefiners. 
+? Esto permite cargar dinámicamente todos los modelos definidos en el directorio /models
+ Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+*/
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
-
-// Injectamos la conexion (sequelize) a todos los modelos
+/*
+? Para cada definición de modelo en modelDefiners, se invoca la función pasando sequelize como argumento. 
+? Esto registra los modelos con Sequelize.   
+ Injectamos la conexion (sequelize) a todos los modelos
+*/
 modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
+/*
+? Luego, se transforman los nombres de los modelos para que comiencen con una letra mayúscula
+ Capitalizamos los nombres de los modelos ie: product => Product
+*/
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
-
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
+/*
+? En sequelize.models están todos los modelos importados como propiedades
+? Para relacionarlos hacemos un destructuring
+*/
 const { Dog } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
+/*
+? Exporta todos los modelos registrados con Sequelize y la conexión sequelize para que puedan ser
+? importados y utilizados en otros archivos de la aplicación. 
+? Esto facilita la interacción con la base de datos desde diferentes partes de la aplicación.
+*/
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
